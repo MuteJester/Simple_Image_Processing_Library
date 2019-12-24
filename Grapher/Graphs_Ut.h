2e7d9901e0363442f2a22a138313c2f79ae90c58
Thomas Konstantinovsky <56339082+MuteJester@users.noreply.github.com>
2019-12-24 20:21:29 +0200

    Update Graphs_Ut.h

--- Grapher/Graphs_Ut.h
+++ Grapher/Graphs_Ut.h
@@ -1,10 +1,34 @@
 #pragma once
 #include "Image_Header.h"
 #include "Color_Catalog.h"
+#include "Mole.h"
 
 typedef struct interval {
 	int Right, Left;
 }interval;
+
+typedef struct Vert {
+	int Right, Left;
+	pixel Color;
+}Vert;
+
+//typedef struct Edge {
+//	int Vr, Vl;
+//}Edge;
+
+class Graph {
+protected:
+	int number_of_vertices;
+	int **edges;
+
+public:
+	Graph(int const &Number_Of_Vertices);
+	void Add_Edge(int const &Vr, int const &Vl);
+	void Print_Edges();
+	void Export_Graph_Image(const char *type);
+
+
+};
 
 class Interval_Graph {
 protected:
